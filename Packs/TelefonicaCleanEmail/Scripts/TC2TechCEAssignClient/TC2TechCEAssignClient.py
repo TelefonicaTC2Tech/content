6e1f09f31a8933e8e62f0e4d19247b0962c5ac39
Sergio Constantino <sergioandrea.constantinonoguera@gmail.com>
2022-10-21 02:23:59 +0200

    Initial commit for CleanEmail content pack

---
+++ Packs/TelefonicaCleanEmail/Scripts/TC2TechCEAssignClient/TC2TechCEAssignClient.py
@@ -0,0 +1,41 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+MODULE_NAME = "TC2TechCEAssignClient"
+
+
+def main(**kwargs):
+    try:
+        inc = demisto.incident()
+        instance = inc["sourceInstance"]
+
+        clients = kwargs["clients"]
+        if isinstance(clients, str):
+            clients = json.loads(clients)
+
+        portal_ids = execute_command(
+            "jmespath",
+            {
+                "value": clients,
+                "expression": f"[?eml.proofpoint.tap.instance=='{instance}'].portal | [0]",
+            },
+        )
+
+        if portal_ids is None:
+            raise Exception(f"Client with instance {instance} not found.")
+
+        cf = inc["CustomFields"]
+        cf |= {
+            "tc2techtenantid": portal_ids["tenant_id"],
+            "tc2techsocid": portal_ids["soc_id"],
+            "tc2techclientid": portal_ids["client_id"],
+        }
+
+        execute_command("setIncident", {"customFields": cf})
+
+    except Exception:
+        return_error("\n".join((MODULE_NAME, "Error:", traceback.format_exc())))
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main(**demisto.args())
