7363de5eb8a8a6b5b2fc4809605bc8c9c5834a14
Sergio Constantino <sergioandrea.constantinonoguera@gmail.com>
2022-10-25 15:58:54 +0200

    OCM Context script

---
+++ Packs/TelefonicaOCM/Scripts/TC2TechOCMContext/TC2TechOCMContext.py
@@ -0,0 +1,75 @@
+import enum
+
+import demistomock as demisto
+from CommonServerPython import *
+
+MODULE_NAME = "TC2TechOCMContext"
+
+
+def coerce(value, _type):
+    if isinstance(value, _type):
+        result = value
+    elif _type in (list, dict):
+        result = json.loads(value)
+    else:
+        raise ValueError(f"The value cannot be coerced to a {str(_type)}.")
+    return result
+
+
+class Operation(enum.Enum):
+    ATTACHMENT_ADD_PORTAL = enum.auto()
+    CANCEL = enum.auto()
+    CLOSE = enum.auto()
+    CREATE = enum.auto()
+    REACTIVATE = enum.auto()
+    REOPEN = enum.auto()
+    RESOLVE = enum.auto()
+    SUSPEND = enum.auto()
+    UPDATE = enum.auto()
+
+
+class SearchClass(enum.Enum):
+    SECURITY = "com.elevenpaths.sandas.input.SecurityAlert"
+    SUPERVISION = "com.elevenpaths.sandas.input.SupervisionAlert"
+
+
+def main(**kwargs):
+    try:
+        inc = demisto.incident()
+        cf = inc["CustomFields"]
+        ocm_model = coerce(cf.get("tc2techmodel", "{}") or "{}", dict)
+        operation = Operation[kwargs["operation"]]
+        search_class = SearchClass[kwargs["type"]]
+        transcode = coerce(kwargs["transcode"], dict)
+
+        context = {
+            "idIncCliente": cf.get("tc2techserviceincidentid"),
+            "searchClass": search_class.value,
+            "operation": operation.name,
+            "lastEventSourceSystem": cf.get("tc2techsourcesystemid"),
+            "securityAlerts": [],
+            "supervisionAlerts": [],
+            "rawEvent": json.dumps(ocm_model),
+            "transcode": transcode,
+        }
+
+        alert_key = None
+        if search_class == SearchClass.SECURITY:
+            alert_key = "securityAlerts"
+        elif search_class == SearchClass.SUPERVISION:
+            alert_key = "supervisionAlerts"
+        context[alert_key].append(ocm_model)
+
+        return_results(
+            CommandResults(
+                outputs_prefix="TC2Tech.OCM.Context",
+                outputs_key_field="idIncCliente",
+                outputs=context,
+            )
+        )
+    except Exception:
+        return_error("\n".join((MODULE_NAME, "Error:", traceback.format_exc())))
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main(**demisto.args())
