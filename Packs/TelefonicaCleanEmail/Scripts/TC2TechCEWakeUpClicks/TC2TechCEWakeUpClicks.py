08028e8688433dc748651243c6ce121d83df2e62
Sergio Constantino <sergioandrea.constantinonoguera@gmail.com>
2022-10-27 12:29:45 +0200

    Wake up clicks

---
+++ Packs/TelefonicaCleanEmail/Scripts/TC2TechCEWakeUpClicks/TC2TechCEWakeUpClicks.py
@@ -0,0 +1,43 @@
+import demistomock as demisto
+from CommonServerPython import *
+
+MODULE_NAME = "TC2TechCEWakeUpClicks"
+
+
+def main():
+    try:
+        inc = demisto.incident()
+        cf = inc["CustomFields"]
+        message_id = cf.get("tc2techcemessageid")
+        clicks = (
+            execute_command(
+                "getIncidents",
+                {
+                    "query": " ".join(
+                        (
+                            'type:"CleanEmail Click"',
+                            f"tc2techcemessageid:{message_id}",
+                            "tc2techceblocked:False" "-status:2",
+                        )
+                    )
+                },
+            )["data"]
+            or []
+        )
+
+        for clk in clicks:
+            execute_command(
+                "taskComplete",
+                {
+                    "id": "WaitForMessage",
+                    "incidentId": clk["id"],
+                    "comment": "Waken up by incident associated to related message.",
+                    "input": "ReadyForUpdate",
+                },
+            )
+    except Exception:
+        return_error("\n".join((MODULE_NAME, "Error:", traceback.format_exc())))
+
+
+if __name__ in ("__main__", "__builtin__", "builtins"):
+    main()
